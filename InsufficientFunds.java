bcaa9959be7e097a90a6a1127d5054b8c0f979de
ReySantosJr <65351134+ReySantosJr@users.noreply.github.com>
2020-08-25 10:41:55 -0400

    Update InsufficientFunds.java

--- InsufficientFunds.java
+++ InsufficientFunds.java
@@ -1,14 +1,4 @@
 package atm;
-
-/**
- * Program Name: ATM Machine
- * File ATMmachine.java
- * File Type: Let's user know of "insufficient funds"
- * Purpose: A mini-program that recreates the basic functions of an ATM machine.
- * 
- * @author Reynaldo Santos
- * @since 2018-11-17
- */
  
 import javax.swing.*;
 
